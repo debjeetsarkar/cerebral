f5a935a48e153f17973529066831423040d010d6
debjeetsarkar <debjeet.sarkar@go-jek.com>
2020-10-08 23:02:01 +0530

    08/10/2020

--- max_valid_parathesis.go
+++ max_valid_parathesis.go
@@ -1,54 +1,79 @@
+/**
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+ 
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+**/
+
 package main
 
-import "fmt"
+
+import (
+	"math"
+	"fmt"
+)
 
 type stack []int
 
-func (stk *stack) push(str int) {
-	*stk = append(*stk, str)
+func (s *stack) push(a int) {
+    *s = append(*s, a)
 }
 
-func (stk *stack) pop() int {
-	if len(*stk) == 0 {
-		return -1
-	}
-	s := (*stk)[len(*stk) - 1]
-	*stk = (*stk)[: len(*stk) - 1]
-	return s
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    
+    return y
 }
 
-func (stk *stack) peek() int{
- if len(*stk) == 0 {
- 	return -2
- }
-  return (*stk)[len(*stk) - 1]
+func (s *stack) pop() int {
+    if len(*s) == 0 {
+        return math.MinInt32
+    }
+
+    popped := (*s)[len(*s) - 1]
+    (*s) = (*s)[0:len(*s) - 1]
+    return popped
+}
+
+func (s *stack) peek() int {
+     if len(*s) == 0 {
+        return math.MinInt32
+    }
+    
+    return (*s)[len(*s)-1]
 }
 
 func longestValidParentheses(s string) int {
- maxLen := 0
- refStack := &stack{-1}
-
- if len(s) == 0 || len(s) == 1 {
- 	return 0
- }
-
- for index, paranthesis := range s {
- 	if string(paranthesis) == "(" {
- 		refStack.push(index)
-	} else if string(paranthesis) == ")" {
-		refStack.pop()
-		if refStack.peek() == -2 {
-			refStack.push(index)
-		} else {
-			if maxLen < index - refStack.peek() {
-				maxLen = index - refStack.peek()
-			}
-		}
-	}
- }
- return maxLen
+    maxLength := 0
+    l := len(s)
+    if l == 0 {
+       return  maxLength
+    }
+    
+    stk := &stack{-1}
+   
+    for index, char := range s {
+        if string(char) == "("{
+            stk.push(index)
+        } else if string(char) == ")" {
+            stk.pop()
+            if stk.peek() != math.MinInt32 {
+                maxLength = max(maxLength, index - stk.peek())    
+            } else {
+                  stk.push(index)  
+            }
+        }
+    }
+    
+    return  maxLength
 }
-
 
 
 func main () {
